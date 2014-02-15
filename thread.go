abd6b2c0a85b4cd2c21658052b6745f7ce4f0c25
snorredc <snorredc@gmail.com>
2014-02-15 02:09:39 +0100

    Improved documentation and added 1-task buffering to Thread.tasks.

--- thread.go
+++ thread.go
@@ -1,29 +1,33 @@
 // Package thread implements simple routines for forcing tasks to be executed
-// on certain threads. Especially useful for code that must be called from the
-// main thread.
+// on certain threads. It is useful for functions that must be run on the same
+// thread, or code that must be run on the main thread (e.g. GUI code under
+// OS X).
 package thread
 
 import (
     "runtime"
 )
 
-// Thread is a handle for an OS thread.
+// Thread is a handle for executing code on an OS thread.
 type Thread struct {
     tasks chan func()
     done  chan struct{}
 }
 
-// NewThread creates a new Thread. The thread will not handles calls to Do
-// until Run has been called.
+// New creates a new Thread. The thread will not process tasks until Run is
+// called.
 func New() *Thread {
     return &Thread{
-        make(chan func()),
+        // Buffering tasks should reduce the required goroutine switches for
+        // single-goroutine-threaded programs.
+        make(chan func(), 1),
         make(chan struct{}),
     }
 }
 
 // Run causes the current thread to execute all functions passed to Do. The
-// thread might still be used randomly by other goroutines.
+// thread might still be used randomly by other goroutines. This calls
+// runtime.LockOSThread but does not unlock it again.
 func (thread *Thread) Run() {
     runtime.LockOSThread()
     for task := range thread.tasks {
@@ -32,8 +36,8 @@ func (thread *Thread) Run() {
     }
 }
 
-// Do causes task to be executed on the Thread and waits for the task to
-// finish.
+// Do causes the task to be executed on the Thread and waits for the task to
+// finish. Do panics, if 
 func (thread *Thread) Do(task func()) {
     thread.tasks <- task
     <-thread.done
